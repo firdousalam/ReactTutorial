dfe8c34b2aff8850f70eb5aa9184c60fa17d5538
firdous alam <firdous.alam2058@gmail.com>
2022-06-13 15:24:39 +0530

    storing data in localstorage

--- reactapp/src/components/App.js
+++ reactapp/src/components/App.js
@@ -1,21 +1,34 @@
-import React,{useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList"
 
 function App() {
-  const [contacts,setContacts] = useState([]);
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+
   const addContactHandler = (contact) => {
-    setContacts([...contacts,contact])
-  }
+    setContacts([...contacts, contact])
+  };
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log("retdata", retriveContacts);
+    if (retriveContacts) {
+      setContacts(retriveContacts);
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+
   return (
     <div className="ui container">
       <Header />
-      <AddContact addContactHandler = {addContactHandler}/>
-      <ContactList contacts={contacts}/>
-
-
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
